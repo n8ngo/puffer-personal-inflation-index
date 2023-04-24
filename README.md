0c89bc706c243edc114258634a00c7fbc202f49a
n8ngo <nathanngo8796@gmail.com>
2023-04-24 12:33:07 -0700

    readme build 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Puffer | A Personal Inflation Index
+
+Inflation is measured by tracking the average change in prices paid by consumers over a period of time for a basket of goods and services. That's great for macroeconomic purposes, but what it doesn't mean much for the average consumer.
+
+Puffer is a tool to track your own set basket of goods over time you can track inflation as it pertains to you. Use this tool to find out that the price of enjoying
--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,15 @@
-import React, { Component, Suspense } from 'react';
-import { useState } from 'react';
-import { AppContainer } from 'react-hot-loader';
+import React, {Component, Suspense} from 'react';
+import {useState} from 'react';
+import {AppContainer} from 'react-hot-loader';
 import Dropdown from './components/Dropdown';
 import AddExpense from './components/AddExpense';
 import ChartData from './components/ChartData';
 import ListFiles from './components/ListFiles';
 import './styles.css';
+import {getComponentTree} from 'react-component-tree';
+const componentTree = getComponentTree(<App />);
+const componentNames = componentTree.children.map((child) => child.name);
+console.log('REACT COMPONENT TREE', componentNames);
 
 /*
  GOALS:
@@ -14,52 +18,63 @@ import './styles.css';
  ADD PIE CHART
 */
 
-const App = props => {
-
+const App = (props) => {
   //FOR ADD EXPENSE
   const [categories, setCategories] = useState([]);
   const [expenseName, setExpenseName] = useState('');
   const [expenseAmount, setExpenseAmount] = useState('');
   const [expenseDate, setExpenseDate] = useState('');
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState('');
   const [expenseNote, setExpenseNote] = useState('');
 
   //FOR CHARTDATA
-  const[data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
   const [selected, setSelected] = useState('Category');
   return (
-    
     <>
-    <Suspense fallback={<div>LOADING</div>}>
-      <header className="header">
-        <img src="./puffer.png" alt="" />
-        <h1>Puffer <span>A personalized inflation tracking tool!</span></h1>
-      </header>
-      <main>
-
-        <div className='graphics'>
-          <ChartData expenseNote={expenseNote} data={data} setData={setData}/>
+      <Suspense fallback={<div>LOADING</div>}>
+        <header className="header">
+          <img src="./puffer.png" alt="" />
+          <h1>
+            Puffer <span>A personalized inflation tracking tool!</span>
+          </h1>
+        </header>
+        <main>
+          <div className="graphics">
+            <ChartData
+              expenseNote={expenseNote}
+              data={data}
+              setData={setData}
+            />
+          </div>
+          <div className="sidebar">
+            <AddExpense
+              categories={categories}
+              expenseName={expenseName}
+              expenseAmount={expenseAmount}
+              expenseDate={expenseDate}
+              category={category}
+              expenseNote={expenseNote}
+              setCategories={setCategories}
+              setExpenseName={setExpenseName}
+              setExpenseAmount={setExpenseAmount}
+              setExpenseDate={setExpenseDate}
+              setCategory={setCategory}
+              setExpenseNote={setExpenseNote}
+            />
+          </div>
+        </main>
+        <footer>
+          <div className="footer">
+            <ListFiles data={data} setData={setData} />
+          </div>
+        </footer>
+        <div className="whywhywhy">
+          <Dropdown selected={selected} setSelected={setSelected} />
         </div>
-        <div className='sidebar'>
-          <AddExpense 
-          categories={categories} expenseName={expenseName} expenseAmount={expenseAmount} expenseDate={expenseDate} category={category} expenseNote={expenseNote}
-          setCategories={setCategories} setExpenseName={setExpenseName} setExpenseAmount={setExpenseAmount} setExpenseDate={setExpenseDate} setCategory={setCategory} setExpenseNote={setExpenseNote}
-          />
-        </div>
-
-      </main>
-      <footer>
-        <div className='footer'>
-          <ListFiles data={data} setData={setData}/>
-        </div>
-      </footer>
-      <div className='whywhywhy'>
-        <Dropdown selected={selected} setSelected={setSelected}/>
-      </div>
       </Suspense>
     </>
-    
   );
 };
 
