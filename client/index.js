7f6e7c209432427882c399f9e25ee5c9efcad8b6
n8ngo <nathanngo8796@gmail.com>
2023-05-02 17:20:49 -0700

    updated goals

--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('app'));
 
 //Add more charts
 //check calculation logic
+// fix app to launch
+// Take screenshots to update readMe
+//
